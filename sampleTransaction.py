9f4b9a50b07c0bc4269589a5e30795d8aa9c1320
shubhdhebar <shubhdhebar@gmail.com>
2022-03-15 11:43:05 +0530

    accounts reconfigured

--- sampleTransaction.py
+++ sampleTransaction.py
@@ -3,10 +3,10 @@ from web3 import Web3
 ganache_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 #Send 1 eth from Acc1 to Acc2
-acc_1 = "0x4EA37346e29cF0092ec5C8Ed7792Efa4288fE669"
-pvt_key = "86527a13d1574691f579f56f90b083944061a5bafbd472dff505db5c04fe5ad4"
+acc_1 = "0x201d4f0E4BD39930Aa905B6A2A2803616dCfaA03"
+pvt_key = "3d1b9180b8c26e2024a4f27ed80c72e9861597a7364b0053f57a511e2d2137e4"
 
-acc_2 = "0xCc72f0be4E2699a2C7b7b97150d97F04D278698f"
+acc_2 = "0x9c9Cc5Fdaf14e3D3Dcdb0E640448ddEBBF686720"
 
 nonce = web3.eth.getTransactionCount(acc_1)
 

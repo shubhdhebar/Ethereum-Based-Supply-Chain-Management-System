507e710206f039b240a0cbdb32d814bbed0a133e
shubhdhebar <shubhdhebar@gmail.com>
2022-03-29 22:16:12 +0530

    contracts added

--- contracts/AssemblyUnit.sol
+++ contracts/AssemblyUnit.sol
@@ -3,26 +3,27 @@ pragma solidity ^0.5.0;
 contract assembly_unit {
     uint batch_id;
     
-    event newBatch(uint process_id, address supplier, address assembler, uint batch_id);
 
-    function orderNewBatch(address supplier, address assembler, uint batchID) public payable{
+    event newBatch(string timestamp, address supplier, address assembler, uint batch_id);
+
+    function orderNewBatch(string memory timestamp, address supplier, address assembler, uint batchID) public returns (uint){
         batchID=batchID+1;
-        emit newBatch(0, supplier, assembler, batchID);
+        emit newBatch(timestamp, supplier, assembler, batchID);
+        return batchID;
     }
 
-    event manufacture(uint process_id, address assembler, address inventory, uint product_id, uint batch_id);
+    event manufacture(string timestamp, address assembler, address inventory, uint product_id, uint batch_id);
 
 
-    function sendToInventory(address assembler, address inventory, uint product_id, address supplier) public payable{
+    function sendToInventory(string memory timestamp, address assembler, address inventory, uint product_id, address supplier) public{
         /*product_id: 1001 01 --> 1001:batch_id*/
         batch_id = product_id/100;
         if (product_id%100==21){
-            orderNewBatch(supplier, assembler, batch_id);
+            batch_id=orderNewBatch(timestamp, supplier, assembler, batch_id);
             product_id=batch_id*100 + 1;
         }
         
-        emit manufacture(1, assembler, inventory, product_id, batch_id);
+        emit manufacture(timestamp, assembler, inventory, product_id, batch_id);
     }
-
     
 }

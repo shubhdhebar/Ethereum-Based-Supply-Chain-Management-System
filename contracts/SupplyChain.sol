ce06c3ad7fe9efb29f62da72f2af887c621f27f3
shubhdhebar <shubhdhebar@gmail.com>
2022-06-01 02:19:18 +0530

    smart contract updated

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -4,6 +4,8 @@ contract supplyChain {
     event newBatch(string timestamp,uint batch_id);
     event manufacture(string timestamp, uint batch_id, uint product_id);
     event qualityControl(string timestamp, uint product_id, uint grade, string comment);
+    event newOrder(string timestamp,uint order_id,uint retailer,uint qty);
+    event dispatch(string timestamp,uint product_id,uint order_id,uint retailer);
 
     function orderNewBatch(string memory timestamp, uint batchID) public returns (uint){
         batchID=batchID+1;
@@ -11,7 +13,6 @@ contract supplyChain {
         return batchID;
     }
 
-    
     function sendToInventory(string memory timestamp, uint product_id) public{
         /*product_id: 1001 01 --> 1001:batch_id*/
         uint batch_id = product_id/100;
@@ -28,5 +29,11 @@ contract supplyChain {
     
     }
 
-    
+    function generateOrder(string memory timestamp,uint order_id,uint retailer,uint qty) public{
+        emit newOrder(timestamp,order_id,retailer,qty);
+    }
+
+    function assignRetailer(string memory timestamp,uint product_id,uint order_id,uint retailer) public{
+        emit dispatch(timestamp,product_id,order_id,retailer);
+    }
 }
